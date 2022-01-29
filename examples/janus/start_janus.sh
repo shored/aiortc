8d9f46792208b186acf6150db643634ec65b2924
Tomohiro Ishihara <sho@c.u-tokyo.ac.jp>
2022-01-29 20:36:50 +0000

    added LD_PRELOAD to prevent link error.

--- examples/janus/start_janus.sh
+++ examples/janus/start_janus.sh
@@ -5,6 +5,6 @@ DURATION=1800
 
 for i in 1 2 3 4
 do
-	ip netns exec test$i /home/ubuntu/aiortc/examples/janus/janus.py --room 1234 --duration $DURATION $SRV_URL &
+	ip netns exec test$i env LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 /home/ubuntu/aiortc/examples/janus/janus.py --room 1234 --duration $DURATION $SRV_URL &
 	sleep 10
 done
