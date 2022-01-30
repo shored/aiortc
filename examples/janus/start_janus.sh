512f89f4265a1375297c9eaa3b1c1a375a487ee5
Tomohiro Ishihara <sho@c.u-tokyo.ac.jp>
2022-01-30 09:34:46 +0000

    changed number of clients.

--- examples/janus/start_janus.sh
+++ examples/janus/start_janus.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 SRV_URL=http://10.249.240.100:8088/janus
-DURATION=1800
+DURATION=900
 IPADDR=$(ip addr show dev eth0 | grep 'inet '| awk '{print $2}')
 
-for i in 1 2 3
+for i in 1 2
 do
 	ip netns exec test$i env LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 /home/ubuntu/aiortc/examples/janus/janus.py --verbose --room 1234 --duration $DURATION --display $IPADDR:$i $SRV_URL 2>&1| grep '<' > $i.result&
 	sleep 10
